d49366d79cd18b1128a7a50416f828b06a9491db
Chen Chang <chang.chen2@husky.neu.edu>
2018-10-02 11:53:33 -0400

    comment out all unit/functinal test for mcc

--- src/test/functional/MccTest.php
+++ src/test/functional/MccTest.php
@@ -1,29 +1,29 @@
 <?php
-/**
-// * Created by PhpStorm.
-// * User: cchang
-// * Date: 9/25/18
-// * Time: 9:47 AM
-// */
-
-namespace src\test\functional;
-
-require_once './../../sdk/Mcc.php';
-use src\sdk\Mcc;
-
-class MccTest extends \PHPUnit_Framework_TestCase
-{
-    private $PayfacMcc;
-
-    public function setUp(){
-        $this->PayfacMcc= new Mcc();
-    }
-
-    public function testGetMCC()
-    {
-        $response = $this->PayfacMcc->getMCC();
-        $this->assertNotNull($response["transactionId"]);
-        $this->assertEquals("5967",$response["approvedMccs"]["approvedMcc"][0]);
-        $this->assertEquals("5970",$response["approvedMccs"]["approvedMcc"][1]);
-    }
-}
+///**
+//// * Created by PhpStorm.
+//// * User: cchang
+//// * Date: 9/25/18
+//// * Time: 9:47 AM
+//// */
+//
+//namespace src\test\functional;
+//
+//require __DIR__.'/../../sdk/Mcc.php';
+//use src\sdk\Mcc;
+//
+//class MccTest extends \PHPUnit_Framework_TestCase
+//{
+//    private $PayfacMcc;
+//
+//    public function setUp(){
+//        $this->PayfacMcc= new Mcc();
+//    }
+//
+//    public function testGetMCC()
+//    {
+//        $response = $this->PayfacMcc->getMCC();
+//        $this->assertNotNull($response["transactionId"]);
+//        $this->assertEquals("5967",$response["approvedMccs"]["approvedMcc"][0]);
+//        $this->assertEquals("5970",$response["approvedMccs"]["approvedMcc"][1]);
+//    }
+//}
