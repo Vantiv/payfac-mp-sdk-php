92d2151ed0577b98750c6102f048683389e3d9cb
Chen Chang <chang.chen2@husky.neu.edu>
2018-10-08 16:05:24 -0400

    change schema path

--- src/utils/Utils.php
+++ src/utils/Utils.php
@@ -130,12 +130,7 @@ class Utils
     public static function validateXML($request){
         $xml = new DOMDocument();
         $xml->loadXML($request);
-//        print "\nschema file location\n";
-        $filepath =  __DIR__."/merchant-onboard-api-v13.xsd";
-//        if(file_exists($filepath))
-//            print "\nyes, the files exists\n";
-//        else
-//            $filepath = __DIR__."/../../schema/merchant-onboard-api-v13.xsd";
+        $filepath =  __DIR__."/../schema/merchant-onboard-api-v13.xsd";
         if(file_exists($filepath)) print "\n\n\nexist!!!!!!!!!!!!!!!\n\n\n";
         return $xml->schemaValidate( $filepath);
     }
